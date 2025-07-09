5947749be7a0a97c209f49c4f32ae49486ee750d
Sai Sri Harsha Pemmasani <sriharshapemmasani06@gmail.com>
2025-07-09 17:49:52 -0400

    updated mobile view

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+  function toggleMenu() {
+      document.querySelector('.menu').classList.toggle('active');
+    }
+
+    fetch("/components/navbar.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("navbar-placeholder").innerHTML = data;
+      });
